83dc52b6541e88cae75cfd6b27b3df6e3046edb6
G-TECH <haya98166@gmail.com>
2024-06-28 13:09:04 +0300

    add featured list view to home

--- lib/Features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/home_view_body.dart
@@ -10,8 +10,26 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
          CustomAppBar(),
-         CustomListViewItem(),
+         FeaturedBoxListView()
       ],
+    );
+  }
+}
+class FeaturedBoxListView extends StatelessWidget {
+  const FeaturedBoxListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height*.3, 
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+      itemBuilder: (context,index){
+        return const Padding(
+          padding:   EdgeInsets.symmetric(horizontal: 8.0),
+          child:  FeaturedListViewItem(),
+        );
+      }),
     );
   }
 }
