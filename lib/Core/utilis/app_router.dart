30c7c032934ebb51431a6356481894fb6fb5073c
G-TECH <haya98166@gmail.com>
2024-06-29 07:37:26 +0300

    create custom book details app bar

--- lib/Core/utilis/app_router.dart
+++ lib/Core/utilis/app_router.dart
@@ -1,13 +1,16 @@
+import 'package:bookly_app/Features/Home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/Features/Home/presentation/views/home_view.dart';
 import 'package:bookly_app/Features/Splash/Presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter{
+  static const homeView='/homeView';
+  static const bookDetailsView='/bookDetailsView';
   static final router=GoRouter(
   routes: [
     GoRoute(path: '/',builder:(context,state)=>const SplashView()),
-    GoRoute(path: '/homeView',builder:(context,state)=>const HomeView()),
-
+    GoRoute(path: homeView,builder:(context,state)=>const HomeView()),
+    GoRoute(path: bookDetailsView ,builder:(context,state)=>const BookDetailsView())
   ]);
 
 }
--- lib/Features/Home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/Home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,55 +1,62 @@
+import 'package:bookly_app/Core/utilis/app_router.dart';
 import 'package:bookly_app/Core/utilis/assets.dart';
 import 'package:bookly_app/Core/utilis/styles.dart';
 import 'package:bookly_app/Features/Home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 160,
-      child: Row(
-        children: [
-          AspectRatio(aspectRatio: 2.5/4,
-          child: Container(
-             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              image: const DecorationImage(
-                image: AssetImage(AssetsData.testImage,),
-                fit:BoxFit.fill
-                ),
-             ),
-          ),),
-          const SizedBox(width: 20,),
-           Expanded(
-             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-              SizedBox(width: MediaQuery.of(context).size.width *.5,
-               child:  Text('Harry Potter and the Goblet of Fire',
-               style:Styles.textStyle20.copyWith(fontFamily: kGtSectraFine,),
-               maxLines: 2,
-               overflow: TextOverflow.ellipsis,
-               )),
-               const SizedBox(height: 3,),
-               const Text('J.K. Rowing',style: Styles.textStyle14,),
-               const SizedBox(height: 3,),
-                Row(
-                  
+    return InkWell(
+      onTap:(){
+        GoRouter.of(context).push(AppRouter.bookDetailsView);
+      },
+      child: SizedBox(
+        height: 160,
+        child: Row(
+          children: [
+            AspectRatio(aspectRatio: 2.5/4,
+            child: Container(
+               decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testImage,),
+                  fit:BoxFit.fill
+                  ),
+               ),
+            ),),
+            const SizedBox(width: 20,),
+             Expanded(
+               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('19,99 €', style: Styles.textStyle20.copyWith(fontWeight:FontWeight.bold),),
-                   const Spacer(),
-                   const BookRating(),
-                ],
-                               ),
-             
-                   
-                       ],),
-           ),
-        ],
+                SizedBox(width: MediaQuery.of(context).size.width *.5,
+                 child:  Text('Harry Potter and the Goblet of Fire',
+                 style:Styles.textStyle20.copyWith(fontFamily: kGtSectraFine,),
+                 maxLines: 2,
+                 overflow: TextOverflow.ellipsis,
+                 )),
+                 const SizedBox(height: 3,),
+                 const Text('J.K. Rowing',style: Styles.textStyle14,),
+                 const SizedBox(height: 3,),
+                  Row(
+                    
+                  children: [
+                    Text('19,99 €', style: Styles.textStyle20.copyWith(fontWeight:FontWeight.bold),),
+                     const Spacer(),
+                     const BookRating(),
+                  ],
+                                 ),
+               
+                     
+                         ],),
+             ),
+          ],
+        ),
       ),
     );
   }
