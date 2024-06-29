75e53fc114f21c1770881b7fd0d732722496e8bb
G-TECH <haya98166@gmail.com>
2024-06-29 08:13:15 +0300

    add rating to book details view

--- lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Core/utilis/styles.dart';
+import 'package:bookly_app/Features/Home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/Features/Home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/Home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
@@ -30,8 +31,8 @@ class BookDetailsViewBody extends StatelessWidget {
               fontWeight: FontWeight.w500
             )),
           ),
-
-         
+          const SizedBox(height: 18,),
+            const BookRating(mainAxisAlignment: MainAxisAlignment.center,)
         ],
       ),
     );
--- lib/Features/Home/presentation/views/widgets/book_rating.dart
+++ lib/Features/Home/presentation/views/widgets/book_rating.dart
@@ -3,18 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating({super.key,  this.mainAxisAlignment=MainAxisAlignment.start});
+final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
-    return  Row(children: [
-      const Icon(
+    return  Row(
+      mainAxisAlignment: mainAxisAlignment,
+      children: const[
+       Icon(
         FontAwesomeIcons.solidStar,color: Color(0xffFFDD4F),
+        size: 16,
       ),
-      const SizedBox(width: 6.3,),
-     const Text('4.8',style: Styles.textStyle16,),
-     const SizedBox(width: 5,),
-     Text('(2359)',style: Styles.textStyle14.copyWith(color:const Color(0xff707070)),),
+       SizedBox(width: 6.3,),
+      Text('4.8',style: Styles.textStyle16,),
+      SizedBox(width: 5,),
+      Opacity(opacity: 0.7,child:  Text('(2390)',style: Styles.textStyle14)),
 
     ],);
   }
