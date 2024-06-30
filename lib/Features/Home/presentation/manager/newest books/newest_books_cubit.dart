09f645464727daf255ba664fc9491df22f8f15bb
G-TECH <haya98166@gmail.com>
2024-06-30 09:13:40 +0300

    create newest books state

---
+++ lib/Features/Home/presentation/manager/newest books/newest_books_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bookly_app/Features/Home/data/models/book_name/book_name.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit() : super(NewestBooksInitial());
+}
